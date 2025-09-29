07ba35724fa73c441d09c98e510adc1b8363e57d
muhammadd-rizall <muhammadrizal1702199@gmail.com>
2025-09-29 17:55:48 +0700

    first commit

---
+++ resources/views/coba.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/css/app.css','resources/js/app.js'])
+</head>
+<body class="bg-gray-100">
+    <h1 class="text-3xl font-bold underline bg-blue-500 text-white p-4">
+        Hello world!
+    </h1>
+</body>
+</html>
