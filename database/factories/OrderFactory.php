e58537cbbbb8b044e88d5663d9b6722d6e26b2cb
muhammadd-rizall <muhammadrizal1702199@gmail.com>
2025-10-12 15:28:38 +0700

    ubah orderFactory dan buat orderseeder

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Customer;
 use App\Models\Diskon;
+use App\Models\Order;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,23 +17,19 @@ class OrderFactory extends Factory
      *
      * @return array<string, mixed>
      */
+    protected $model = Order::class;
+
     public function definition(): array
     {
-       $customerIds = Customer::pluck('id')->all();
+        $customerIds = Customer::pluck('id')->all();
         $diskonIds = Diskon::pluck('id')->all();
-
-        $totalHarga = $this->faker->randomFloat(2, 50000, 1000000);
-
-        // Tentukan apakah ada diskon atau tidak
-        $diskonId = $this->faker->optional()->randomElement($diskonIds);
-        $totalDiskon = $diskonId ? $this->faker->randomFloat(2, 5000, min(50000, $totalHarga)) : 0;
 
         return [
             'customer_id' => $this->faker->randomElement($customerIds),
-            'diskon_id' => $diskonId,
-            'total_harga' => $totalHarga,
-            'total_diskon' => $totalDiskon,
-            'harga_akhir' => $totalHarga - $totalDiskon,
+            'diskon_id' => $this->faker->optional()->randomElement($diskonIds),
+            'total_harga_awal' => 0,   // akan dihitung di seeder
+            'total_diskon' => 0,       // akan dihitung di seeder
+            'total_harga_akhir' => 0,  // akan dihitung di seeder
             'alamat_pengiriman' => $this->faker->address(),
             'catatan' => $this->faker->optional()->sentence(),
             'status' => $this->faker->randomElement(['pending', 'proses', 'dikirim', 'cancelled']),
