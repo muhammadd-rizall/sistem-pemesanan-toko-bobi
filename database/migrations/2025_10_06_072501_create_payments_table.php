090f2b464713a4165b63ff8bb14e2d44e1d7a2af
muhammadd-rizall <muhammadrizal1702199@gmail.com>
2025-10-12 11:52:48 +0700

    memperbaiki databse payment dan order

--- database/migrations/2025_10_06_072501_create_payments_table.php
+++ database/migrations/2025_10_06_072501_create_payments_table.php
@@ -14,10 +14,16 @@ return new class extends Migration
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('order_id')->constrained('orders')->onDelete('cascade');
-            $table->decimal('jumlah_bayar', 10, 2);
+            $table->enum('jenis_pembayaran', ['full', 'dp'])->default('full');
+            $table->decimal('total_order', 10, 2);
+            $table->decimal('jumlah_terbayar', 10, 2)->default(0);
+            $table->decimal('sisa_pembayaran', 10, 2)->default(0);
             $table->enum('metode_pembayaran', ['credit_card', 'bank_transfer', 'e_wallet', 'cash_on_delivery', 'qris'])->default('bank_transfer');
+            $table->string('bukti_pembayaran')->nullable();
             $table->dateTime('tanggal_bayar');
             $table->enum('status', ['pending', 'completed', 'failed'])->default('pending');
+
+
             $table->timestamps();
         });
     }
