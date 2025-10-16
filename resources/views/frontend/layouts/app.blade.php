a29fb894c2cfa6ffcd3850a5133b6096ec89a6dc
raidatulzukira <raidatulzukiraa@gmail.com>
2025-10-16 18:11:21 +0700

    mengupdate tampilan home dan halaman produk pada landing page

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -75,7 +75,6 @@
 <body class="bg-white text-gray-900 antialiased">
 
     <!-- Header -->
-    <!-- Header -->
 <header class="bg-sage-200 sticky top-0 z-50 transition-all duration-300 border-b border-sage-100" id="navbar">
     <nav class="container mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between items-center py-4 sm:py-5">
@@ -122,20 +121,22 @@
                 </a>
             </div>
 
+            <!-- Pemisah -->
+                {{-- <div class="hidden lg:block w-px h-6 bg-sage-300 mx-4"></div> --}}
+
             <!-- Right Actions (Tombol di Kanan) -->
             <div class="flex items-center">
+                <div class="hidden lg:block w-px h-6 bg-sage-300 mx-4"></div>
                 <!-- Tombol Login (Desktop) -->
-                <div class="hidden lg:flex items-center gap-2">
+                <div class="hidden lg:flex items-center gap-2 mr-12">
                     <a href="{{ route('login') }}" class="px-5 py-2.5 text-base font-medium text-white bg-sage-600 hover:bg-sage-700 rounded-full transition-colors duration-300 shadow-sm">
                         Masuk
                     </a>
                 </div>
 
-                <!-- Pemisah -->
-                <div class="hidden lg:block w-px h-6 bg-sage-300 mx-4"></div>
 
                 <!-- Ikon Kanan -->
-                <div class="flex items-center gap-2 sm:gap-4">
+                {{-- <div class="flex items-center gap-2 sm:gap-4">
                     <button class="p-2 sm:p-2.5 text-sage-700 hover:text-sage-600 hover:bg-sage-50 rounded-full transition-all duration-300">
                         <svg class="w-5 h-5 sm:w-6 sm:h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/></svg>
                     </button>
@@ -145,7 +146,7 @@
                     <button class="lg:hidden p-2 text-sage-700 hover:text-sage-600 hover:bg-sage-50 rounded-lg transition-all duration-300" onclick="toggleMobileMenu()">
                         <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"/></svg>
                     </button>
-                </div>
+                </div> --}}
             </div>
         </div>
 
