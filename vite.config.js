ee20cb969eb46da3331ca5fcd497b1f7e55ac172
muhammadd-rizall <muhammadrizal1702199@gmail.com>
2025-11-18 09:52:12 +0700

    Add build manifest for CSS and JS assets

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-// import tailwindcss from '@tailwindcss/vite';
-
-// import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
     plugins: [
@@ -10,6 +7,5 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        // tailwindcss(),
     ],
 });
